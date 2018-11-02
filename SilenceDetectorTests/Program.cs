cfcdd5307facd1cf385dc37abeac7577ff72ec8c
Unknown <mr.i.d.fernando@gmail.com>
2018-11-02 12:08:25 -0500

    Audible splitting functional

--- SilenceDetectorTests/Program.cs
+++ SilenceDetectorTests/Program.cs
@@ -9,6 +9,7 @@ namespace SilenceDetectorTests
     {
         static void Main(string[] args)
         {
+            /*
             List<TimeInterval> silenceIntervals = new List<TimeInterval>
             {
                 new TimeInterval(0, 0.1),
@@ -19,11 +20,15 @@ namespace SilenceDetectorTests
             };
 
             List<TimeInterval> audibleTimeIntervals = Helpers.ExtractAudibleTimeIntervals(silenceIntervals);
-
+            
             var dummy = 15;
+            */
+            string ffmpegExecutablePath = @"c:\tools\ffmpegnew.exe";
+            string inputFilename = @"c:\temp\audiblesplit\SoundTest.mp4";
+            double minimalSilenceIntervalToCut = 1;
+            double audibleClipSilencePadding = 0.2;
 
-            string inputFilename = @"D:/folder/subfolder/myfile.mp4";
-
+            AudibleDetectorExecutor.Execute(ffmpegExecutablePath, inputFilename, minimalSilenceIntervalToCut, audibleClipSilencePadding);
 
             Console.ReadLine();
         }
